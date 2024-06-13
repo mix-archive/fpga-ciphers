be1d3525c431b694fcd369fccf9acb85804a341a
Mix <32300164+mnixry@users.noreply.github.com>
2024-06-13 18:44:56 +0800

    refactor: improve sm3 testbench verilog

--- 05_SM3/test.v
+++ 05_SM3/test.v
@@ -7,6 +7,7 @@ module test_sm3;
   reg [31:0] din;
   reg [2:0] cmd_i;
   reg cmd_w_i;
+  reg [511:0] y;
 
   wire [31:0] dout;
   wire [3:0] cmd_o;
@@ -29,6 +30,7 @@ module test_sm3;
     reset = 1'b0;
     cmd_w_i = 1'b0;
     cmd_i = 3'b0;
+    y = 512'b0;
 
     #21 reset = 1'b1;
 
@@ -37,141 +39,21 @@ module test_sm3;
     @(posedge clk);
 
     cmd_i   = 3'b010;
-    cmd_w_i = 1'b1;
-
-    @(posedge clk);
-    cmd_w_i = 1'b0;
-    din = 32'h61626380;
-
-    @(posedge clk);
-    din = 32'h0;
-
-    @(posedge clk);
-    din = 32'h0;
-
-    @(posedge clk);
-    din = 32'h0;
-
-    @(posedge clk);
-    din = 32'h0;
-
-    @(posedge clk);
-    din = 32'h0;
-
-    @(posedge clk);
-    din = 32'h0;
-
-    @(posedge clk);
-    din = 32'h0;
-
-    @(posedge clk);
-    din = 32'h0;
-
-    @(posedge clk);
-    din = 32'h0;
-
-    @(posedge clk);
-    din = 32'h0;
-
-    @(posedge clk);
-    din = 32'h0;
-
-    @(posedge clk);
-    din = 32'h0;
-
-    @(posedge clk);
-    din = 32'h0;
-
-    @(posedge clk);
-    din = 32'h0;
-
-    @(posedge clk);
-    din = 32'd24;
-
-    @(posedge clk);
-    @(posedge clk);
-    @(posedge clk);
-    @(posedge clk);
-    @(posedge clk);
-
+    y = {"abc", 8'h80, 416'd0, 61'd3, 3'd0};
+    //y = {"abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq", 8'h80, 56'd0};
+    inputdata(y);
+    repeat(5) @(posedge clk);
     while (cmd_o[3]) @(posedge clk);
+    repeat(5) @(posedge clk);
 
-    @(posedge clk);
-    @(posedge clk);
-    @(posedge clk);
-    @(posedge clk);
-    @(posedge clk);
+    //#100;
+//    cmd_i = 3'b110;
+//    y = {448'h0, 61'd56, 3'd0};
+//    inputdata(y);
+//    repeat(5) @(posedge clk);
+//    while (cmd_o[3]) @(posedge clk);
+//    repeat(5) @(posedge clk);
 
-    #100;
-
-    /*@(posedge clk);
-        cmd_i = 3'b110;
-        cmd_w_i = 1'b1;
- 
-        @(posedge clk);
-        cmd_w_i = 1'b0;
-        din=32'h80000000;
-        
-        @(posedge clk);
-        din=32'b0;
-        
-        @(posedge clk);
-        din=32'b0;
-        
-        @(posedge clk);
-        din=32'b0;
-        
-        @(posedge clk);
-        din=32'b0;
-        
-        @(posedge clk);
-        din=32'b0;
-        
-        @(posedge clk);
-        din=32'b0;
-        
-        @(posedge clk);
-        din=32'b0;
-        
-        @(posedge clk);
-        din=32'b0;
-        
-        @(posedge clk);
-        din=32'b0;
-        
-        @(posedge clk);
-        din=32'b0;
-        
-        @(posedge clk);
-        din=32'b0;
-        
-        @(posedge clk);
-        din=32'b0;
-        
-        @(posedge clk);
-        din=32'b0;
-        
-        @(posedge clk);
-        din=32'b0;
-        
-        @(posedge clk);
-        din=32'd512;
-        
-        @(posedge clk);
-        @(posedge clk);
-        @(posedge clk);
-        @(posedge clk);
-        @(posedge clk);
-       
-        while (cmd_o[3])
-		@(posedge clk);
- 
-        @(posedge clk);
-        @(posedge clk);
-        @(posedge clk);
-        @(posedge clk);
-        @(posedge clk);
-        */
     cmd_i   = 3'b001;
     cmd_w_i = 1'b1;
 
@@ -189,7 +71,21 @@ module test_sm3;
     @(posedge clk);
 
     #100;
+    
 
 
   end
+  task inputdata;
+    input [511:0] y;
+    integer i;
+
+    begin
+      cmd_w_i = 1'b1;
+      for (i = 0; i <= 15; i = i + 1) begin
+        @(posedge clk);
+        din = (y >> 512 - ((i + 1) * 32));
+        cmd_w_i = 1'b0;
+      end
+    end
+  endtask
 endmodule
